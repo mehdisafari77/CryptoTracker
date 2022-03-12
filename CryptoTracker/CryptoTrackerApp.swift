8148dc785b5e6e5eee8db7c4ffc63ee96c49a9bb
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-11 22:22:36 -0800

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Mehdi MMS on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
