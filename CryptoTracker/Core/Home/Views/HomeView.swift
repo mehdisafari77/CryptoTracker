05b82e03642d559dd8ed4c7bdf99595dfbfcb771
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-12 21:10:15 -0800

    create circleButtons

--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // Content Layer
+            VStack {
+                Text("Header")
+                
+                Spacer(minLength: 0)
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
     }
 }
