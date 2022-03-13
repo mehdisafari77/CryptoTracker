ed8201ca740f4a447f2379657295547f850af720
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-12 21:25:40 -0800

    add custom animation for buttons

--- CryptoTracker/Core/Components/CircleButton/CircleButtonAnimationView.swift
+++ CryptoTracker/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -10,17 +10,24 @@ import SwiftUI
 struct CircleButtonAnimationView: View {
     
     
-    @State pribate var animate: Bool = false
+    @State private var animate: Bool
     
     var body: some View {
         Circle()
             .stroke(lineWidth: 5.0)
             .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0)
+            .animation(animate ? Animation.easeOut(duration: 1.0) : .none)
+            .onAppear {
+                animate.toggle()
+            }
     }
 }
 
 struct CircleButtonAnimationView_Previews: PreviewProvider {
     static var previews: some View {
         CircleButtonAnimationView()
+            .foregroundColor(.red)
+            .frame(width: 100, height: 100)
     }
 }
