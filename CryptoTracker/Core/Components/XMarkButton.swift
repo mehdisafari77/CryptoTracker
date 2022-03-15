6d19e99ee8411a302dcc3bddf888ae5c9372efde
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-14 20:42:45 -0700

    custom x mark button created

---
+++ CryptoTracker/Core/Components/XMarkButton.swift
@@ -0,0 +1,32 @@
+//
+//  XMarkButton.swift
+//  CryptoTracker
+//
+//  Created by Mehdi MMS on 14/03/2022.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+
+    
+    var body: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        })
+    }
+}
+
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XMarkButton()
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
+
