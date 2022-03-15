a935b82f355119051f4ce2da08f7ba8c07918b0f
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-14 20:15:40 -0700

    created necessary extension in portfolio

--- CryptoTracker/Core/Home/Views/PortfolioView.swift
+++ CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -40,7 +40,7 @@ struct PortfolioView: View {
                     trailingNavBarButton
                 }
             })
-            //            .navigationBarItems(leading: XMarkButton()) // depricated, use like above tool bar
+//            .navigationBarItems(leading: XMarkButton()) // depricated, use like above tool bar
             .onChange(of: vm.searchText, perform: { value in
                 if value == "" {
                     removeSelectedCoin()
@@ -53,5 +53,132 @@ struct PortfolioView: View {
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
+            .environmentObject(dev.homeVM)
     }
 }
+
+extension PortfolioView {
+    
+    private var coinLogoList: some View {
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .onTapGesture {
+                                withAnimation(.easeIn) {
+                                    updateSelectedCoin(coin: coin)
+                                }
+                        }
+                        .padding(4)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(
+                                    selectedCoin?.id == coin.id ? Color.theme.green : Color.clear,
+                                    lineWidth: 1.0
+                                )
+                        )
+                }
+            }
+            .frame(height: 120)
+            .padding(.leading)
+        }
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+
+        if let portfolioCoin = vm.portfolioCoins.first(where: { $0.id == coin.id }),
+           let amount = portfolioCoin.currentHoldings {
+            quantityText = "\(amount)"
+        } else {
+            quantityText = ""
+        }
+    }
+    
+    private func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText) {
+            return quantity * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
+    }
+    
+    private var portfolioInputSection: some View {
+        VStack(spacing: 20) {
+            HStack {
+                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? "") :")
+                Spacer()
+                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+            }
+            Divider()
+            HStack {
+                Text("Amount holding:")
+                Spacer()
+                TextField("Ex: 1.4", text: $quantityText)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
+          }
+            Divider()
+            HStack {
+                Text("Current value:")
+                Spacer()
+                Text(getCurrentValue().asCurrencyWith2Decimals())
+            }
+            Divider()
+        }
+        .animation(.none)
+        .padding()
+        .font(.headline)
+    }
+    
+    private var trailingNavBarButton: some View {
+        HStack(spacing: 10) {
+            Image(systemName: "checkmark")
+                .opacity(showCheckmark ? 1.0 : 0.0)
+            Button(action: {
+                saveButtonTapped()
+            }, label: {
+                Text("Save".uppercased())
+            })
+            .opacity(
+                (selectedCoin != nil && selectedCoin?.currentHoldings != Double(quantityText)) ?
+                    1.0 : 0.0
+            )
+        }
+        .font(.headline)
+    }
+    
+    private func saveButtonTapped() {
+        
+        guard
+            let coin = selectedCoin,
+            let amount = Double(quantityText)
+            else { return }
+        
+        // save to portfolio [ using core data container ]
+        vm.updatePortfolio(coin: coin, amount: amount)
+        
+        // show checkmark
+        withAnimation(.easeIn) {
+            showCheckmark = true
+            removeSelectedCoin()
+        }
+        
+        // hide keyboard
+        UIApplication.shared.endEditing()
+        
+        // hide checkmark
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0 ) {
+            withAnimation(.easeOut) {
+                showCheckmark = false
+            }
+        }
+    }
+    
+    private func removeSelectedCoin() {
+        selectedCoin    = nil
+        vm.searchText   = ""
+    }
+}
+
