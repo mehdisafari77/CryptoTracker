6d19d66296e667265f399d4c9624508bae281719
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-14 20:11:18 -0700

    create home stats view

---
+++ CryptoTracker/Core/Home/Views/HomeStatsView.swift
@@ -0,0 +1,35 @@
+//
+//  HomeStatsView.swift
+//  CryptoTracker
+//
+//  Created by Mehdi MMS on 14/03/2022.
+//
+
+import SwiftUI
+
+struct HomeStatsView: View {
+    
+    @EnvironmentObject var vm: HomeViewModel
+    
+    @Binding var showPortfolio: Bool
+    
+    var body: some View {
+        HStack {
+            ForEach(vm.statistics) { stat in
+                StatisticView(stat: stat)
+                    .frame(width: UIScreen.main.bounds.width / 3)
+            }
+        }
+        .frame(
+            width: UIScreen.main.bounds.width,
+            alignment: showPortfolio ? .trailing : .leading
+        )
+    }
+}
+
+struct HomeStatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeStatsView(showPortfolio: .constant(false))
+            .environmentObject(dev.homeVM)
+    }
+}
