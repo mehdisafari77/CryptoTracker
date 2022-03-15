ae73dd3b476a11ca66050352b8f6f20f630f2433
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-14 20:13:13 -0700

    create portfolio view

---
+++ CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioView.swift
+//  CryptoTracker
+//
+//  Created by Mehdi MMS on 14/03/2022.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PortfolioView_Previews: PreviewProvider {
+    static var previews: some View {
+        PortfolioView()
+    }
+}
