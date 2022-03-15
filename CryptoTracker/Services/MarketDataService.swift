7bb24f5a9f1fb8dab89db3a120f89c0af84c5c8e
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-14 21:03:05 -0700

    market data service Implemented

--- CryptoTracker/Services/MarketDataService.swift
+++ CryptoTracker/Services/MarketDataService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel?
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    func getData() {
+         
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else {
+            return
+        }
+        
+        marketDataSubscription = NetworkManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
