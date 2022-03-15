7efb6f413d91b64107ab779db99e937b0861e00a
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-14 20:58:48 -0700

    coin data service finalized

--- CryptoTracker/Services/CoinDataService.swift
+++ CryptoTracker/Services/CoinDataService.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+        
+    // but problem , difficult to figure out the correct one to cancel.
+    // var cancallables = Set<AnyCancellable>()
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    func getCoins() {
+         
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
+        )
+        else {
+            return
+        }
+        
+        coinSubscription = NetworkManager.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            })
+    }
+}
